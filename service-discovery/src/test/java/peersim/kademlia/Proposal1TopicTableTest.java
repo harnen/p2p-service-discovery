d8e95b51d87f1c6079e32504ab9a3052894b6ff3
srenevic <s.rene@ucl.ac.uk>
2020-07-02 16:37:11 +0200

    tests setup

--- service-discovery/src/test/java/peersim/kademlia/Proposal1TopicTableTest.java
+++ service-discovery/src/test/java/peersim/kademlia/Proposal1TopicTableTest.java
@@ -1,36 +1,86 @@
 package peersim.kademlia;
 
 import java.math.BigInteger;
+import java.util.Random;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
 
 import peersim.kademlia.Proposal1TopicTable;
 import peersim.kademlia.UniformRandomGenerator;
+import peersim.core.CommonState;
+import peersim.config.ParsedProperties;
+import peersim.config.Configuration;
 
 
-public class Proposal1TopicTableTest extends TestCase{
-    Proposal1TopicTable tt1;
-    Proposal1TopicTable tt2;
-
-    protected void setUp() {
-        tt1 = new Proposal1TopicTable(new BigInteger("100000000000000000000000000000000000000000000000"));
-        tt2 = new Proposal1TopicTable(new BigInteger("999999999999999999999999999999999999999999999999"));
-
-        tt1.setCapacity(2);
-        tt2.setCapacity(5);
+public class Proposal1TopicTableTest{
+    
+    @BeforeAll
+    protected static void setUpBeforeClass() {
+        String[] array = new String[] {"config/simple.cfg"};
+        Configuration.setConfig( new ParsedProperties(array) );
+        CommonState.setEndTime(Long.parseLong("100"));
+        CommonState.setTime(Long.parseLong("0"));
+        
     }
 
-    public void testCapacity() {	
+    @Test
+    public void capacityTopics() {
+        Proposal1TopicTable tt = new Proposal1TopicTable(new BigInteger("100000000000000000000000000000000000000000000000"));
+        UniformRandomGenerator urg = new UniformRandomGenerator(KademliaCommonConfig.BITS, 1);
+        tt.setCapacity(3);
+
         for(int i = 0; i < 10; i++){
-            UniformRandomGenerator urg = new UniformRandomGenerator(KademliaCommonConfig.BITS, 1);
+            
             Topic t = new Topic(new BigInteger("0"), "topic"+i);
             Registration r = new Registration(urg.generate());
-            Boolean result1 = tt1.register(r, t);
-            Boolean result2 = tt2.register(r, t);
+            //System.out.println("Adding topic:" +  t + "with registration:" + r);
+            Boolean result = tt.register(r, t);
         }
-        assert(tt1.getSize() == tt1.getCapacity());
-        assert(tt2.getSize() == tt2.getCapacity());
+        //System.out.println("tt:" + tt);
+        assert(tt.getSize() == tt.getCapacity());
+
+
     }
-    
+
+    @Test
+    public void capacityRegistrations() {	
+        Proposal1TopicTable tt = new Proposal1TopicTable(new BigInteger("100000000000000000000000000000000000000000000000"));
+        UniformRandomGenerator urg = new UniformRandomGenerator(KademliaCommonConfig.BITS, 1231);
+        tt.setCapacity(5);
+
+        Topic t = new Topic(new BigInteger("0"), "topic");
+        for(int i = 0; i < 10; i++){
+            Registration r = new Registration(urg.generate());
+            //System.out.println("Adding topic:" +  t + "with registration:" + r);
+            Boolean result1 = tt.register(r, t);
+            
+        }
+        //System.out.println("tt:" + tt);
+        assert(tt.getSize() == tt.getCapacity());
+    }
+
+    @Test
+    public void sameRegistrations() {	
+        Proposal1TopicTable tt = new Proposal1TopicTable(new BigInteger("100000000000000000000000000000000000000000000000"));
+        UniformRandomGenerator urg = new UniformRandomGenerator(KademliaCommonConfig.BITS, 1231);
+        tt.setCapacity(3);
+
+        Topic t = new Topic(new BigInteger("0"), "topic");
+        Registration r = new Registration(urg.generate());
+        for(int i = 0; i < 3; i++){
+            //System.out.println("Adding topic:" +  t + "with registration:" + r);
+            Boolean result1 = tt.register(r, t);
+            //System.out.println("tt:" + tt);
+        }
+        //System.out.println("tt:" + tt);
+        assert(tt.getSize() == 1);
+    }
+
+
 }
--- service-discovery/src/test/java/peersim/kademlia/TopicTest.java
+++ service-discovery/src/test/java/peersim/kademlia/TopicTest.java
@@ -1,23 +1,23 @@
 package peersim.kademlia;
 
+
 import java.math.BigInteger;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
 
-public class TopicTest extends TestCase {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TopicTest{
     Topic t1, t2;
 
-    protected void setUp() {
+     @Test
+    public void testComparison() {
         BigInteger hostID = new BigInteger("100000000000000000000000000000000000000000000000");
         t1 = new Topic(hostID, "topic1");
-        System.out.println("t1:" + t1);
-        t2 = new Topic(hostID, "topic1");
-        System.out.println("t2:" + t2);
-     }
-
-    public void testComparison() {
-        assertTrue(t1.compareTo(t2) == 1);
+        //System.out.println("t1:" + t1);
+        t2 = new Topic(hostID, "topic2");
+        assertEquals(t1.compareTo(t2), 1);
    }
 
 
