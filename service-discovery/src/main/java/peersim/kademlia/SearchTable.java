0423ce04074af62c369867726a6e089653bf01b4
srenevic <s.rene@ucl.ac.uk>
2020-09-28 14:37:09 +0200

    ticket table + search table for ticket protocol - need to check topic lookup

---
+++ service-discovery/src/main/java/peersim/kademlia/SearchTable.java
@@ -0,0 +1,87 @@
+package peersim.kademlia;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+import peersim.core.CommonState;
+
+public class SearchTable extends RoutingTable {
+
+    /**
+	 * Table to keep track of topic registrations
+	 */
+    private List<BigInteger> pendingTickets;
+    
+    private Discv5TicketProtocol protocol;
+    
+    private Topic t;
+    
+    private int myPid;
+    
+    boolean pendingLookup;
+    
+	public SearchTable(int nBuckets, int k, int maxReplacements,Discv5TicketProtocol protocol,Topic t, int myPid) {
+		
+		super(nBuckets, k, maxReplacements);
+		
+		//pendingTickets = new ArrayList<BigInteger>();
+
+		this.protocol = protocol;
+		
+		this.t = t;
+		
+		this.nodeId = t.getTopicID();
+		
+		this.myPid = myPid;
+		
+		pendingLookup = true;
+		// TODO Auto-generated constructor stub
+	}
+	
+	// add a neighbour to the correct k-bucket
+	public void addNeighbour(BigInteger[] node) {
+		for(BigInteger dest : node)
+			super.addNeighbour(dest);
+		
+		if(pendingLookup) {
+			pendingLookup = false;
+
+        	Message m = new Message(Message.MSG_INIT_TOPIC_LOOKUP, t);
+    		m.timestamp = CommonState.getTime();
+			protocol.sendTopicLookup(m, t, myPid);
+		}
+
+	}
+
+
+	// remove a neighbour from the correct k-bucket
+	public void removeNeighbour(BigInteger node) {
+		// get the lenght of the longest common prefix (correspond to the correct k-bucket)
+		//pendingTickets.remove(node);
+		bucket(node).removeNeighbour(node);
+	}	
+	/**
+	 * Check nodes and replace buckets with valid nodes from replacement list
+	 * 
+	 */
+	public void refreshBuckets(int kademliaid) {
+		//System.out.println("Refresh bucket");
+		//System.out.println("Routingtable refreshBuckkets "+CommonState.getTime());
+		Random rnd= new Random();
+		//for(int i=0;i<nBuckets;i++) {
+		KBucket b = k_buckets[rnd.nextInt(nBuckets)];
+		if(b.neighbours.size()<k)
+			protocol.sendLookup(t, myPid);
+		if(b.neighbours.size()>0) {
+			b.checkAndReplaceLast(kademliaid);
+			return;
+		}
+	}
+	
+	
+
+
+}
---
+++ service-discovery/src/main/java/peersim/kademlia/TicketTable.java
@@ -0,0 +1,101 @@
+package peersim.kademlia;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+public class TicketTable extends RoutingTable {
+
+    /**
+	 * Table to keep track of topic registrations
+	 */
+    private List<BigInteger> pendingTickets;
+    
+    private Discv5TicketProtocol protocol;
+    
+    private Topic t;
+    
+    private int myPid;
+    
+	public TicketTable(int nBuckets, int k, int maxReplacements,Discv5TicketProtocol protocol,Topic t, int myPid) {
+		
+		super(nBuckets, k, maxReplacements);
+		
+		pendingTickets = new ArrayList<BigInteger>();
+
+		this.protocol = protocol;
+		
+		this.t = t;
+		
+		this.nodeId = t.getTopicID();
+		
+		this.myPid = myPid;
+		// TODO Auto-generated constructor stub
+	}
+	
+	// add a neighbour to the correct k-bucket
+	public void addNeighbour(BigInteger[] nodes) {
+		for(BigInteger node : nodes) {
+			//System.out.println("Add node "+node+" to bucket "+Util.logDistance(node, nodeId));
+
+			if(!pendingTickets.contains(node)) {
+				if(super.addNeighbour(node)) {
+					pendingTickets.add(node);
+					//pendingTickets.put(node, null);
+					protocol.sendTicketRequest(node,t,myPid);
+				}
+			}
+		}
+	}
+
+	public void addTicket(Message m,Ticket ticket) {
+		//System.out.println("Ticket added for topic "+t.getTopic());
+		if(pendingTickets.contains(m.src.getId())) {
+			//System.out.println("Ticket added for topic "+t.getTopic());
+			//pendingTickets.put(m.src.getId(), ticket);
+			
+		    Message register = new Message(Message.MSG_REGISTER, t);
+			register.ackId = m.id; // set ACK number
+		    register.dest = new KademliaNode(m.src);
+		    register.body = m.body;
+		    register.operationId = m.operationId;
+			protocol.scheduleSendMessage(register, m.src.getId(), myPid, ticket.getWaitTime());
+		}
+			
+	}
+	
+	// remove a neighbour from the correct k-bucket
+	public void removeNeighbour(BigInteger node) {
+		// get the lenght of the longest common prefix (correspond to the correct k-bucket)
+		pendingTickets.remove(node);
+		bucket(node).removeNeighbour(node);
+	}	
+	/**
+	 * Check nodes and replace buckets with valid nodes from replacement list
+	 * 
+	 */
+	/**
+	 * Check nodes and replace buckets with valid nodes from replacement list
+	 * 
+	 */
+	public void refreshBuckets(int kademliaid) {
+		//System.out.println("Routingtable refreshBuckkets "+CommonState.getTime());
+		Random rnd= new Random();
+		//for(int i=0;i<nBuckets;i++) {
+		KBucket b = k_buckets[rnd.nextInt(nBuckets)];
+		if(b.neighbours.size()<k)
+			protocol.sendLookup(t, myPid);
+		if(b.neighbours.size()>0) {
+			b.checkAndReplaceLast(kademliaid);
+			return;
+		}
+		//}
+	}
+	
+	
+	
+
+
+}
