5b343b3d09021082638ae94012982a99d25e2ef3
sergi <sergi@srenevic.net>
2020-12-12 12:32:40 +0100

    randomness in init registrations

--- service-discovery/src/main/java/peersim/kademlia/Discv5MultiTopicTrafficGenerator.java
+++ service-discovery/src/main/java/peersim/kademlia/Discv5MultiTopicTrafficGenerator.java
@@ -112,10 +112,11 @@ public class Discv5MultiTopicTrafficGenerator extends Discv5ZipfTrafficGenerator
                     System.out.println();
 				    if(registerMessage != null) {
 					    //int time = CommonState.r.nextInt(900000);
-					    int time = 0;
+						int time = CommonState.r.nextInt(KademliaCommonConfig.AD_LIFE_TIME);
+					    //int time = 0;
 					    System.out.println("Topic " + topicList[topicIndex-1].getTopic() + " will be registered by "+prot.getNode().getId()+" at "+time);
 					    EDSimulator.add(time, registerMessage, start, start.getKademliaProtocol().getProtocolID());
-					    EDSimulator.add(time+2000, lookupMessage, start, start.getKademliaProtocol().getProtocolID());
+					    EDSimulator.add(KademliaCommonConfig.AD_LIFE_TIME+time, lookupMessage, start, start.getKademliaProtocol().getProtocolID());
 
 				    }
 			    }
