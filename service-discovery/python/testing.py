46ce1c7b79e104ecbc7dfbeabfd037dbe1b7942f
Michał Król <mharnen@gmail.com>
2021-04-20 18:38:45 +0100

    testing table params

--- service-discovery/python/testing.py
+++ service-discovery/python/testing.py
@@ -1,10 +1,10 @@
 from table import get_entropy
 import matplotlib.pyplot as plt
 import math
-import random
+from numpy import random
 import pandas as pd
 
-def plot_multi(data, tics, cols=None, spacing=.1, **kwargs):
+def plot_multi(data, cols=None, spacing=.1, **kwargs):
 
     from pandas import plotting
 
@@ -12,12 +12,11 @@ def plot_multi(data, tics, cols=None, spacing=.1, **kwargs):
     if cols is None: cols = data.columns
     if len(cols) == 0: return
     #colors = getattr(getattr(plotting, '_style'), '_get_standard_colors')(num_colors=len(cols))
-    colors = ['green', 'blue', 'orange', 'red']
+    colors = ['green', 'blue', 'orange', 'red', 'black', 'brown']
 
     # First axis
     ax = data.loc[:, cols[0]].plot(label=cols[0], color=colors[0], style= '-', **kwargs)
     ax.set_ylabel(ylabel=cols[0])
-    ax.set_xlabel('Items/Users')
     lines, labels = ax.get_legend_handles_labels()
 
     print(cols)
@@ -51,8 +50,8 @@ def get_entropy_modifier(topics, topic):
         current_topic_entropy = get_entropy(topics)
         new_topics = topics + [topic]
         new_topic_entropy = get_entropy(new_topics)
-        
-        if((len(topics) - 1) == 0):
+        print(topics)
+        if((len(topics)) == 0):
             topic_modifier = 1
         elif(new_topic_entropy == 0):
             topic_modifier = 2
@@ -62,6 +61,7 @@ def get_entropy_modifier(topics, topic):
         print("old->new_entropy:", current_topic_entropy, "->", new_topic_entropy, "new - old:", new_topic_entropy - current_topic_entropy, "new/old:", new_topic_entropy/current_topic_entropy, "modifier:", topic_modifier)
         topic_modifier = max(topic_modifier, 1)
         return topic_modifier
+        #return new_topic_entropy
 
 def get_polynomial_modifier(topics, topic):
     count = topics.count(topic)
@@ -70,26 +70,26 @@ def get_polynomial_modifier(topics, topic):
 def get_random_modifier(topics, topic):
     return random.randint(3, 9)
 
-def test_topic_modifier():
-    input = ['t1', 't2','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t2','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1','t1']
-    modifiers = {}
-    modifiers['n'] = list(range(0, len(input)))
-    modifiers['entropy'] = []
-    modifiers['polynomial'] = []
-    modifiers['random'] = []
-    topics = []
-    for item in input:
-        modifiers['entropy'].append(get_entropy_modifier(topics, item))
-        modifiers['polynomial'].append(get_polynomial_modifier(topics, item))
-        modifiers['random'].append(get_random_modifier(topics, item))
-        topics.append(item)
-    print("Input:", input)
-    print("Modifiers:", modifiers)
+def test_topic_modifier(inputs):
     
+    modifiers = {}
+    modifiers['n'] = list(range(0, len(inputs[list(inputs)[0]])))
+    for input_name in inputs.keys():
+        topics = []
+        input = inputs[input_name]
+        modifiers['entropy_'+input_name] = []
+        #modifiers['polynomial_'+input_name] = []
+        for item in input:
+            modifiers['entropy_'+input_name].append(get_entropy_modifier(topics, item))
+            #modifiers['polynomial_'+input_name].append(get_polynomial_modifier(topics, item))
+            topics.append(item)
+    #print("Input:", input)
+    print("Modifiers:", modifiers)
+
     df = pd.DataFrame(modifiers)
     df.set_index('n', inplace=True)
     print(df)
-    plot_multi(df, input, figsize=(6, 3))
+    plot_multi(df, figsize=(6, 3))
     plt.show()
     #figure, ax = plt.subplots()
     #ax.plot(range(0, len(modifiers['entropy'])), modifiers['entropy'], label='entropy')
@@ -98,4 +98,12 @@ def test_topic_modifier():
     #ax.set_title("Modifiers")
     #plt.show()
 
-test_topic_modifier()
+size = 100
+inputs = {}
+inputs['all_same'] = ['t1']*size
+inputs['one_different'] = ['t1']*size
+inputs['one_different'][int(size/5)] = 't2'
+inputs['zipf'] = random.zipf(a=2, size=size)
+inputs['all_different'] = list(range(0, size))
+
+test_topic_modifier(inputs)
