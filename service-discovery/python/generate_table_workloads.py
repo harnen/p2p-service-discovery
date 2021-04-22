26a27a56a7dd77a146cb911c4f34bd0de329f054
OnurAscigil <o.ascigil@ucl.ac.uk>
2021-04-22 10:28:17 +0100

    added time and attack flag to workloads

--- service-discovery/python/generate_table_workloads.py
+++ service-discovery/python/generate_table_workloads.py
@@ -1,40 +1,43 @@
 from numpy import random
 import csv
+import random as rand
 
-def generate_regular(size = 100, zipf_distribution = 2):
+def generate_regular(size = 100, zipf_distribution = 2, rate = 1.0, seed = 0.0):
     output_filename = './workloads/regular_size' + str(size) + '_dist' + str(zipf_distribution) + '.csv'
     #get ips/ids from ethereum repo
     ip_file = open('./workloads/ips.txt', "r")
     id_file = open('./workloads/ids.txt', "r")
+    rand.seed(seed)
     topics = random.zipf(a=zipf_distribution, size=size)#for topics
+    t_next_req = 0.0 # time of next request
     with open(output_filename, 'w') as output_file:
-        fieldnames = ['id', 'ip', 'topic']
+        fieldnames = ['time', 'id', 'ip', 'topic', 'attack']
         dict_writer = csv.DictWriter(output_file, fieldnames=fieldnames)
         dict_writer.writeheader()
         for i in range(0, size):
+            t_next_req += rand.expovariate(rate)
             record = {}
             ip = ip_file.readline().rstrip()
-            id = id_file.readline().rstrip()
-            if(not ip or not id):
+            iD = id_file.readline().rstrip()
+            if(not ip or not iD):
                 print("Not enough IPs/IDs in the files")
                 exit(1)
-            record['id'] = id
+            record['time'] = t_next_req
+            record['id'] = iD
             record['ip'] =ip
             record['topic'] = 't' + str(topics[i])
+            record['attack'] = 0
             #print(record)
             dict_writer.writerow(record)
     print("Generated regular workload in", str(output_filename))
 
-def generate_attack_topic(size = 100, zipf_distribution = 2, topic_to_attack = 't11', attacker_ip_num = 3, attacker_id_num=10, percentage_malicious = 0.9):
+def generate_attack_topic(size = 100, zipf_distribution = 2, topic_to_attack = 't11', attacker_ip_num = 3, attacker_id_num=10, rate_normal = 1.0, rate_attack = 10.0, seed = 0.0):
     output_filename = './workloads/attack_topic_size' + str(size) + '_dist' + str(zipf_distribution) + '.csv'
     #get ips/ids from ethereum repo
     ip_file = open('./workloads/ips.txt', "r")
     id_file = open('./workloads/ids.txt', "r")
     topics = random.zipf(a=zipf_distribution, size=size)#for topics
 
-    honest_percentage = 1-percentage_malicious
-    entry_counter =  int((honest_percentage/2) * size)
-    exit_counter = int(entry_counter + (percentage_malicious * size))
     attacker_ips = []
     for i in range(0, attacker_ip_num):
         num = int(255/attacker_ip_num * i)
@@ -44,46 +47,61 @@ def generate_attack_topic(size = 100, zipf_distribution = 2, topic_to_attack = '
     attacker_ids = []
     for i in range(0, attacker_id_num):
         attacker_ids.append(''.join([str(i)]*20))
-    print("entry_counter:", entry_counter, "exit_counter:", exit_counter)
     print("attacker ips:", attacker_ips)
     print("attacker ids:", attacker_ids)
 
+    rand.seed(seed)
+    t_next_normal_req = rand.expovariate(rate_normal)  # time of next normal request
+    t_next_attack_req = rand.expovariate(rate_attack) # time of next attack request
+    time = 0.0
+    attack = 0
     with open(output_filename, 'w') as output_file:
-        fieldnames = ['id', 'ip', 'topic']
+        fieldnames = ['time', 'id', 'ip', 'topic', 'attack']
         dict_writer = csv.DictWriter(output_file, fieldnames=fieldnames)
         dict_writer.writeheader()
         for i in range(0, size):
+            if t_next_normal_req < t_next_attack_req:
+                attack = 0
+                time = t_next_normal_req
+            elif t_next_normal_req > t_next_attack_req:
+                attack = 1
+                time = t_next_attack_req
+
             record = {}
-            if(i < entry_counter or  i >= exit_counter ):
+            if( attack == 0 ):
                 ip = ip_file.readline().rstrip()
-                id = id_file.readline().rstrip()
-                if(not ip or not id):
+                iD = id_file.readline().rstrip()
+                if(not ip or not iD):
                     print("Not enough IPs/IDs in the files")
                     exit(1)
                 topic = 't' + str(topics[i])
-            else:
+            else: # attack == 1
                 ip = attacker_ips[i % attacker_ip_num]
-                id = attacker_ids[i % attacker_id_num]
+                iD = attacker_ids[i % attacker_id_num]
                 topic = topic_to_attack
 
-            record['id'] = id
+            record['time'] = time 
+            record['id'] = iD
             record['ip'] =ip
             record['topic'] = topic
+            record['attack'] = attack
             #print(record)
             dict_writer.writerow(record)
+        
+            if time == t_next_normal_req:
+                t_next_normal_req += rand.expovariate(rate_normal)
+            if time == t_next_attack_req:
+                t_next_attack_req += rand.expovariate(rate_attack)
     print("Generated regular workload in", str(output_filename))
 
 
-def generate_spam_topic(size = 100, zipf_distribution = 2, topic_to_attack = 't1', attacker_ip_num = 3, attacker_id_num=10, percentage_malicious = 0.9):
+def generate_spam_topic(size = 100, zipf_distribution = 2, topic_to_attack = 't1', attacker_ip_num = 3, attacker_id_num=10, rate_normal = 1.0, rate_attack = 10.0, seed = 0.0):
     output_filename = './workloads/spam_topic_size' + str(size) + '_dist' + str(zipf_distribution) + '.csv'
     #get ips/ids from ethereum repo
     ip_file = open('./workloads/ips.txt', "r")
     id_file = open('./workloads/ids.txt', "r")
     topics = random.zipf(a=zipf_distribution, size=size)#for topics
 
-    honest_percentage = 1-percentage_malicious
-    entry_counter =  int((honest_percentage/2) * size)
-    exit_counter = int(entry_counter + (percentage_malicious * size))
     attacker_ips = []
     for i in range(0, attacker_ip_num):
         num = int(255/attacker_ip_num * i)
@@ -93,39 +111,52 @@ def generate_spam_topic(size = 100, zipf_distribution = 2, topic_to_attack = 't1
     attacker_ids = []
     for i in range(0, attacker_id_num):
         attacker_ids.append(''.join([str(i)]*20))
-    print("entry_counter:", entry_counter, "exit_counter:", exit_counter)
     print("attacker ips:", attacker_ips)
     print("attacker ids:", attacker_ids)
+    
+    rand.seed(seed)
+    t_next_normal_req = rand.expovariate(rate_normal)  # time of next normal request
+    t_next_attack_req = rand.expovariate(rate_attack) # time of next attack request
+    time = 0.0
+    attack = 0
 
     with open(output_filename, 'w') as output_file:
-        fieldnames = ['id', 'ip', 'topic']
+        fieldnames = ['time', 'id', 'ip', 'topic', 'attack']
         dict_writer = csv.DictWriter(output_file, fieldnames=fieldnames)
         dict_writer.writeheader()
         for i in range(0, size):
+            if t_next_normal_req < t_next_attack_req:
+                attack = 0
+                time = t_next_normal_req
+            elif t_next_normal_req > t_next_attack_req:
+                attack = 1
+                time = t_next_attack_req
             record = {}
-            if(i < entry_counter or  i >= exit_counter ):
+            if( attack == 0 ): # normal traffic
                 ip = ip_file.readline().rstrip()
-                id = id_file.readline().rstrip()
-                if(not ip or not id):
+                iD = id_file.readline().rstrip()
+                if(not ip or not iD):
                     print("Not enough IPs/IDs in the files")
                     exit(1)
                 topic = 't' + str(topics[i])
-            else:
+            else: # attack traffic
                 ip = attacker_ips[i % attacker_ip_num]
-                id = attacker_ids[i % attacker_id_num]
+                iD = attacker_ids[i % attacker_id_num]
                 topic = 't' + str(100+i)
 
-            record['id'] = id
+            record['time'] = time 
+            record['id'] = iD
             record['ip'] =ip
             record['topic'] = topic
+            record['attack'] = attack
             #print(record)
             dict_writer.writerow(record)
     print("Generated regular workload in", str(output_filename))
 
 zipf_distribution = 2
-size = 10
+size = 100
 
 #generate_attack_topic(size=1000)
-#generate_spam_topic(size=1000)
-generate_regular(size = size, zipf_distribution=zipf_distribution)
+generate_spam_topic(size=1000)
+#generate_regular(size = size, zipf_distribution=zipf_distribution)
 
