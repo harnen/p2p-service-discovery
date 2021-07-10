b4345a81e55fad5d6c17f211c780538789da4331
oascigil <onur.ascigil@gmail.com>
2021-07-10 11:00:16 +0100

    additional RTT in detecting better waiting time occurrence

--- service-discovery/src/main/java/peersim/kademlia/Discv5EvilTicketProtocol.java
+++ service-discovery/src/main/java/peersim/kademlia/Discv5EvilTicketProtocol.java
@@ -364,7 +364,7 @@ public class Discv5EvilTicketProtocol extends Discv5TicketProtocol {
         	
         	if(previousTicketRequestTime.get(topic).get(m.src.getId())!=null) {
         		//logger.warning("Previous ticket "+previousTicketRequestTime.get(topic).get(m.src.getId())+" previous time "+initTicketRequestTime.get(topic).get(m.src.getId())+" current ticket wait "+ticket.getWaitTime());
-        		if((CommonState.getTime()-initTicketRequestTime.get(topic).get(m.src.getId())>0)&&(previousTicketRequestTime.get(topic).get(m.src.getId())>(CommonState.getTime()-initTicketRequestTime.get(topic).get(m.src.getId())+ticket.getWaitTime()))) {
+        		if(( CommonState.getTime() - initTicketRequestTime.get(topic).get(m.src.getId()) > 0) && ( previousTicketRequestTime.get(topic).get(m.src.getId()) > (CommonState.getTime()-initTicketRequestTime.get(topic).get(m.src.getId()) + ticket.getWaitTime() + ticket.getRTT()) ) ) {
                     logger.warning("Received smaller waiting time than before "+ticket.getWaitTime()+" "+previousTicketRequestTime.get(topic).get(m.src.getId())+" "+CommonState.getTime()+" "+initTicketRequestTime.get(topic).get(m.src.getId()));
                     KademliaObserver.reportBetterWaitingTime(ticket.getTopic().getTopic(),previousTicketRequestTime.get(topic).get(m.src.getId()),ticket.getWaitTime(),CommonState.getTime()-initTicketRequestTime.get(topic).get(m.src.getId()));
         		}
