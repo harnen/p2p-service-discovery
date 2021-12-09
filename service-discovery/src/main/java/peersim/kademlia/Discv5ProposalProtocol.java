b350587820001ff56afc85cb9a075d1afe06862d
Sergi Rene <sergi@datahop.network>
2021-12-09 12:53:10 +0100

    discv5 dht missing files

---
+++ service-discovery/src/main/java/peersim/kademlia/Discv5ProposalProtocol.java
@@ -0,0 +1,388 @@
+package peersim.kademlia;
+
+
+import java.math.BigInteger;
+import java.util.HashSet;
+
+import peersim.config.Configuration;
+import peersim.core.CommonState;
+import peersim.core.Network;
+import peersim.core.Node;
+import peersim.edsim.EDSimulator;
+import peersim.kademlia.operations.LookupOperation;
+import peersim.kademlia.operations.Operation;
+import peersim.kademlia.operations.RegisterOperation;
+import peersim.transport.UnreliableTransport;
+
+
+
+
+public class Discv5ProposalProtocol extends KademliaProtocol {
+
+	public Discv5ProposalTopicTable topicTable;
+	final String PAR_TOPIC_TABLE_CAP = "TOPIC_TABLE_CAP";
+
+	public Discv5ProposalProtocol(String prefix) {
+		super(prefix);
+		this.topicTable = new Discv5ProposalTopicTable();
+
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * Replicate this object by returning an identical copy.<br>
+	 * It is called by the initializer and do not fill any particular field.
+	 * 
+	 * @return Object
+	 */
+	public Object clone() {
+		Discv5ProposalProtocol dolly = new Discv5ProposalProtocol(Discv5ProposalProtocol.prefix);
+		return dolly;
+	}
+	
+    /**
+	 * This procedure is called only once and allow to inizialize the internal state of KademliaProtocol. Every node shares the
+	 * same configuration, so it is sufficient to call this routine once.
+	 */
+	protected void _init() {
+		// execute once
+		if (_ALREADY_INSTALLED)
+			return;
+
+		
+		KademliaCommonConfig.TOPIC_TABLE_CAP = Configuration.getInt(prefix + "." + PAR_TOPIC_TABLE_CAP, KademliaCommonConfig.TOPIC_TABLE_CAP);
+		
+		super._init();
+	}
+	
+	
+	protected void handleTopicQueryReply(Message m, int myPid) {
+		LookupOperation lop = (LookupOperation) this.operations.get(m.operationId);
+		if (lop == null) {
+			return;
+		}
+		
+		BigInteger[] neighbours = ((Message.TopicLookupBody) m.body).neighbours;
+		TopicRegistration[] registrations = ((Message.TopicLookupBody) m.body).registrations;
+		lop.elaborateResponse(neighbours);
+		for(BigInteger neighbour: neighbours)
+			routingTable.addNeighbour(neighbour);
+		
+
+		for(TopicRegistration r: registrations) {
+			lop.addDiscovered(r.getNode(),m.src.getId());
+			KademliaObserver.addDiscovered(lop.topic, this.node.getId(), r.getNode().getId());
+		}
+		
+		lop.increaseReturned(m.src.getId());
+		if(!lop.finished)lop.increaseUsed(m.src.getId());
+
+		//System.out.println("Topic query reply received for "+lop.topic.getTopic()+" "+this.getNode().getId()+" "+lop.discoveredCount()+" "+lop.getUsedCount()+" "+lop.getReturnedCount());
+
+		
+		int found = lop.discoveredCount();
+		int all = KademliaObserver.topicRegistrationCount(lop.topic.topic);
+		int required = KademliaCommonConfig.TOPIC_PEER_LIMIT;//Math.min(all, KademliaCommonConfig.TOPIC_PEER_LIMIT);
+		if(!lop.finished && found >= required) {
+			logger.warning("Found " + found + " registrations out of required " + required + "(" + all + ") for topic " + lop.topic.topic);
+			lop.finished = true;
+		}
+
+		while ((lop.available_requests > 0)) { // I can send a new find request
+			// get an available neighbour
+			BigInteger neighbour = lop.getNeighbour();
+			if (neighbour != null) {
+				if(!lop.finished) {
+					// send a new request only if we didn't find the node already
+					Message request = new Message(Message.MSG_REGISTER);
+					request.operationId = lop.operationId;
+					request.type = Message.MSG_TOPIC_QUERY;
+					request.src = this.node;
+					request.body = lop.body;
+					request.dest = new KademliaNode(neighbour);
+	
+					if(request != null) {
+						lop.nrHops++;
+						sendMessage(request, neighbour, myPid);
+					}
+				}else {
+					//getNeighbour decreases available_requests, but we didn't send a message
+					lop.available_requests++;
+				}
+					
+			} else if (lop.available_requests == KademliaCommonConfig.ALPHA) { // no new neighbour and no outstanding requests
+				// search operation finished
+				operations.remove(lop.operationId);
+				//lop.visualize();
+				logger.warning("reporting operation " + lop.operationId);
+				KademliaObserver.reportOperation(lop);
+				//lop.visualize(); uncomment if you want to see visualization of the operation
+				if(!lop.finished) { 
+					logger.warning("Found only " + found + " registrations out of " + all + " for topic " + lop.topic.topic);
+				}
+				//System.out.println("Writing stats");
+				KademliaObserver.register_total.add(all);
+				KademliaObserver.register_ok.add(found);
+									
+				node.setLookupResult(lop.getDiscovered(),lop.topic.getTopic());
+				return;
+			} else { // no neighbour available but exists oustanding request to wait
+				return;
+			}
+		}
+	}
+	
+
+	
+	private void handleInitTopicLookup(Message m, int myPid) {
+		KademliaObserver.lookup_total.add(1);
+		
+		Topic t = (Topic) m.body;
+	
+		//System.out.println("Send topic lookup for topic "+t.getTopic());
+
+		LookupOperation lop = new LookupOperation(this.node.getId(), m.timestamp, t);
+		lop.body = m.body;
+		lop.type = Message.MSG_TOPIC_QUERY;
+		operations.put(lop.operationId, lop);
+	
+		int distToTopic = Util.logDistance((BigInteger) t.getTopicID(), this.node.getId());
+		BigInteger[] neighbours = this.routingTable.getNeighbours(distToTopic);
+		
+		
+		if(neighbours.length<KademliaCommonConfig.ALPHA)
+			neighbours = this.routingTable.getKClosestNeighbours(KademliaCommonConfig.ALPHA, distToTopic);
+		
+		lop.elaborateResponse(neighbours);
+		lop.available_requests = KademliaCommonConfig.ALPHA;
+	
+		// set message operation id
+		m.operationId = lop.operationId;
+		m.type = Message.MSG_TOPIC_QUERY;
+		m.src = this.node;
+	
+		// send ALPHA messages
+		for (int i = 0; i < KademliaCommonConfig.ALPHA; i++) {
+			BigInteger nextNode = lop.getNeighbour();
+			if (nextNode != null) {
+				m.dest = new KademliaNode(nextNode);
+				sendMessage(m.copy(), nextNode, myPid);
+				lop.nrHops++;
+			}
+		}
+		
+	}
+
+
+
+	/**
+	 * Start a register opearation.<br>
+	 * Find the ALPHA closest node and send register request to them.
+	 * 
+	 * @param m
+	 *            Message received (contains the node to find)
+	 * @param myPid
+	 *            the sender Pid
+	 */
+	protected void handleInitRegister(Message m, int myPid) {
+		Topic t = (Topic) m.body;
+		TopicRegistration r = new TopicRegistration(this.node, t);
+    	//System.out.println("Sending topic registration for topic "+t.getTopic());
+
+		KademliaObserver.addTopicRegistration(t, this.node.getId());
+	
+		RegisterOperation rop = new RegisterOperation(this.node.getId(), m.timestamp, t, r);
+		rop.body = m.body;
+		rop.type = Message.MSG_REGISTER;
+		operations.put(rop.operationId, rop);
+		
+		int distToTopic = Util.logDistance((BigInteger) t.getTopicID(), this.node.getId());
+		BigInteger[] neighbours = this.routingTable.getNeighbours(distToTopic);
+		
+		if(neighbours.length < KademliaCommonConfig.ALPHA)
+			neighbours = this.routingTable.getKClosestNeighbours(KademliaCommonConfig.ALPHA, distToTopic);
+
+		rop.elaborateResponse(neighbours);
+		rop.available_requests = KademliaCommonConfig.ALPHA;
+	
+		m.operationId = rop.operationId;
+		m.type = Message.MSG_REGISTER;
+		m.src = this.node;
+	
+		// send ALPHA messages
+		for (int i = 0; i < KademliaCommonConfig.ALPHA; i++) {
+			BigInteger nextNode = rop.getNeighbour();
+			//System.out.println("Nextnode "+nextNode);
+			if (nextNode != null) {
+				m.dest = new KademliaNode(nextNode);
+				sendMessage(m.copy(), nextNode, myPid);
+				rop.nrHops++;
+			}//nextNode may be null, if the node has less than ALPHA neighbours
+		}
+	}
+	
+
+	/**
+	 * Response to a register request.<br>
+	 * Tries to register the requesting node under the
+	 * specified topic
+	 * 
+	 * @param m
+	 *            Message
+	 * @param myPid
+	 *            the sender Pid
+	 */
+	private void handleRegister(Message m, int myPid) {
+		Topic t = (Topic) m.body;
+		TopicRegistration r = new TopicRegistration(m.src, t);
+        Message response; 
+
+		if(this.topicTable.register(r, t)) {
+			logger.info(t.topic + " registered on " + this.node.getId() + " by " + m.src.getId());
+            response = new Message(Message.MSG_REGISTER_RESPONSE, t);
+            response.ackId = m.id;
+    		response.operationId = m.operationId;
+            response.dest = m.src;
+            response.src = this.node;
+	    	assert m.src != null;
+    		logger.info(" responds with REGISTER_RESPONSE");
+            sendMessage(response, m.src.getId(), myPid);
+		}
+
+		handleFind(m, myPid, Util.logDistance(t.getTopicID(), this.node.getId()));
+    }
+     
+     /**
+     * Process a register response message.<br>
+     * The body should contain a ticket, which indicates whether registration is 
+     * complete. In case it is not, schedule sending a new register request
+     * 
+     * @param m
+     *            Message received (contains the node to find)
+     * @param myPid
+     *            the sender Pid
+     */
+    protected void handleRegisterResponse(Message m, int myPid) {
+        Topic t = (Topic) m.body;
+
+        KademliaObserver.reportActiveRegistration(t,this.node.is_evil);
+        
+        KademliaObserver.addAcceptedRegistration(t, this.node.getId(),m.src.getId(),CommonState.getTime());
+
+    }   
+
+	protected void handleTopicQuery(Message m, int myPid) {
+		
+		Topic t = (Topic) m.body;
+		TopicRegistration[] registrations = this.topicTable.getRegistration(t);
+		BigInteger[] neighbours = this.routingTable.getNeighbours(Util.logDistance(t.getTopicID(), this.node.getId()));
+	
+		//System.out.println("Topic query received at node "+this.node.getId()+" "+registrations.length+" "+neighbours.length);
+
+		Message.TopicLookupBody body = new Message.TopicLookupBody(registrations, neighbours);
+		Message response  = new Message(Message.MSG_TOPIC_QUERY_REPLY, body);
+		response.operationId = m.operationId;
+		response.src = this.node;
+		assert m.src != null;
+		response.dest = m.src;
+		response.ackId = m.id; 
+		logger.info(" responds with TOPIC_QUERY_REPLY");
+		sendMessage(response, m.src.getId(), myPid);
+		
+	}
+	
+	private void handleTimeout(Timeout t, int myPid){
+		Operation op = this.operations.get(t.opID);
+		if(op!=null) {	
+			//logger.warning("Timeout "+t.getType());
+			BigInteger unavailableNode = t.node;
+			if(op.type == Message.MSG_TOPIC_QUERY) {
+				Message m = new Message();
+				m.operationId = op.operationId;
+				m.type = Message.MSG_TOPIC_QUERY_REPLY;
+				m.src = new KademliaNode (unavailableNode);
+				m.dest = this.node;
+				m.ackId = t.msgID; 
+				m.body=  new Message.TopicLookupBody(new TopicRegistration [0], new BigInteger[0]);
+				handleTopicQueryReply(m, myPid);
+			}
+		}
+	}
+	
+	
+	/**
+	 * manage the peersim receiving of the events
+	 * 
+	 * @param myNode
+	 *            Node
+	 * @param myPid
+	 *            int
+	 * @param event
+	 *            Object
+	 */
+	public void processEvent(Node myNode, int myPid, Object event) {
+		// Parse message content Activate the correct event manager fot the particular event
+		super.processEvent(myNode, myPid, event);
+		
+		
+		if(((SimpleEvent) event).getType() == Timeout.TIMEOUT) {
+			handleTimeout((Timeout) event, myPid);
+			return;
+		}
+		Message m = (Message) event;
+		m.dest = this.node;
+		
+		if (m.src != null) {
+			routingTable.addNeighbour(m.src.getId());
+		}
+
+
+		switch (((SimpleEvent) event).getType()) {
+			case Message.MSG_TOPIC_QUERY_REPLY:
+				sentMsg.remove(m.ackId);
+				handleTopicQueryReply(m, myPid);
+				break;
+			
+			case Message.MSG_REGISTER:
+				handleRegister(m, myPid);
+				break;
+				
+			case Message.MSG_INIT_REGISTER:
+				handleInitRegister(m, myPid);
+				break;			
+				
+			case Message.MSG_TOPIC_QUERY:
+				handleTopicQuery(m, myPid);
+				break;
+				
+			case Message.MSG_INIT_TOPIC_LOOKUP:
+				handleInitTopicLookup(m, myPid);
+				break;
+            case Message.MSG_REGISTER_RESPONSE:
+                handleRegisterResponse(m, myPid);
+                break;
+	
+			case Message.MSG_EMPTY:
+				// TO DO
+				break;
+	
+			case Message.MSG_STORE:
+				// TO DO
+				break;
+		}
+
+	}
+	
+	/**
+	 * set the current NodeId
+	 * 
+	 * @param tmp
+	 *            BigInteger
+	 */
+	public void setNode(KademliaNode node) {
+		this.topicTable.setHostID(node.getId());
+		super.setNode(node);
+		
+	}
+
+}
---
+++ service-discovery/src/main/java/peersim/kademlia/Discv5ProposalTopicTable.java
@@ -0,0 +1,194 @@
+package peersim.kademlia;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.math.BigInteger;
+
+import peersim.kademlia.Topic;
+import peersim.core.CommonState;
+import peersim.kademlia.TopicRegistration;
+
+
+public class Discv5ProposalTopicTable implements TopicTable {
+
+
+    private int capacity = KademliaCommonConfig.TOPIC_TABLE_CAP;
+    private int size = 0;
+
+    private SortedMap<Topic, List<TopicRegistration>> table;
+    private BigInteger hostID;
+    
+
+    public Discv5ProposalTopicTable(BigInteger hostID){
+        table = new TreeMap<Topic, List<TopicRegistration>>();
+        this.hostID = hostID;
+    }
+
+    public Discv5ProposalTopicTable(){
+        table = new TreeMap<Topic, List<TopicRegistration>>();
+    }
+
+    private void add(TopicRegistration r, Topic t){
+        if(!table.containsKey(t)){
+            List list = new ArrayList<TopicRegistration>();
+            list.add(r);
+            table.put(t, list);
+        }else{
+            table.get(t).add(r);
+        }
+        this.size++;
+    }
+
+    public boolean register(TopicRegistration ri, Topic ti){
+        //need to create a copy here. Without it - the topic/registration class would be shared among 
+        //all the class where it's registered
+        Topic t = new Topic(ti);
+        t.setHostID(this.hostID);
+        TopicRegistration r = new TopicRegistration(ri);
+        r.setTimestamp(CommonState.getTime());
+
+        //check if we already have this registration
+        List<TopicRegistration> regList = table.get(t);
+        if((regList != null) && (regList.contains(r))){
+        	//System.out.println("We already have topic " + t.getTopic());
+            return true;
+        }
+        
+        //if we have space, always add the registration
+        if(size < capacity){
+            //System.out.println(hostID + "Size lower than capacity - adding");
+            add(r, t);
+            return true;
+        //table is full
+        }else{
+            //new topic is further closer/equal distance from the hostID than the furthest one currently in table
+            if(t.compareTo(table.lastKey()) >= 0){
+            	//System.out.println("The topic is closer than another one - replacing");
+                table.get(table.lastKey()).remove(0);
+                //if a topic has no more registration - remove it
+                if(table.get(table.lastKey()).size() == 0) table.remove(table.lastKey());
+                this.size--;
+                add(r, t);
+                
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    public TopicRegistration[] getRegistration(Topic t){
+    	Topic t1 = new Topic(t);
+    	t1.hostID = this.hostID;
+        if(table.containsKey(t1)){
+        	List<TopicRegistration> list;
+        	if(table.get(t1).size()>=16)
+        		list = table.get(t1).subList(0, 16);
+        	else 
+        		list = table.get(t1);
+            return (TopicRegistration[]) list.toArray(new TopicRegistration[list.size()]);
+        }
+        
+        return new TopicRegistration[0];
+    }
+    
+    public HashMap<Topic,Integer> getRegbyTopic(){
+        HashMap<Topic,Integer> regByTopic = new HashMap<Topic,Integer>();
+        for(List<TopicRegistration> t: table.values())
+        {
+        	regByTopic.put(t.get(0).getTopic(), t.size());
+        }
+        
+        return regByTopic;
+
+    }
+    
+    public HashMap<BigInteger,Integer> getRegbyRegistrant(){
+        HashMap<BigInteger,Integer> regByRegistrant = new HashMap<BigInteger,Integer>();
+    	 for(List<TopicRegistration> t: table.values())
+         {
+    		for(TopicRegistration reg : t)
+    		{
+    			int count=0;
+    			if(regByRegistrant.get(reg.getNode().getId())!=null)count=regByRegistrant.get(reg.getNode().getId());
+    			count++;
+    			regByRegistrant.put(reg.getNode().getId(),count);
+    		}
+         }
+    	 //System.out.println("Table "+hostID+" "+count);
+        return regByRegistrant;
+
+    }
+    
+    public int getRegbyRegistrar(){
+    	int count=0;
+    	 for(List<TopicRegistration> t: table.values())
+         {
+         	count+=t.size();
+         }
+    	 //System.out.println("Table "+hostID+" "+count);
+        return count;
+
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
+
+    public void setCapacity(int capacity){
+        this.capacity = capacity;
+    }
+    
+    public int getSize(){
+        return this.size;
+    }
+
+    public void setHostID(BigInteger id){
+        this.hostID = id;
+    }
+
+    public BigInteger getHostID(){
+        return this.hostID;
+    }
+
+    public void clear(){
+        this.table.clear();
+        this.size = 0;
+    }
+
+    public String toString(){
+        //need a final variable inside lambda expressions below
+        final StringBuilder result = new StringBuilder();
+        result.append("--------------------------------\n");
+        result.append("Proposal1Topic Table size: " + this.size + "/" + this.capacity + " hostID: " + this.hostID);
+        this.table.forEach((k, v) -> {
+            
+            result.append("\n" + k.toString() + ":");
+            v.forEach((TopicRegistration reg) ->{
+                result.append(" " + reg.toString());
+            });
+        });
+        result.append("\n--------------------------------");
+        return result.toString();
+    }
+    
+
+    
+    public String dumpRegistrations() {
+    	String result = "";
+    	for(Topic topic: table.keySet()) {
+    		List<TopicRegistration> regList = table.get(topic);
+    		for(TopicRegistration reg: regList) {
+    			result += this.hostID + ",";
+    			result += reg.getTopic().getTopic() + ",";
+    			result += reg.getNode().getId()+ ",";
+    			result += reg.getTimestamp() +"\n";
+    		}	
+    	}
+    	return result;
+    }
+    
+}
