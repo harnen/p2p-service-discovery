0f1b926101e8f39ea1122d6bce9421bb5d9db1b5
Sergi Rene <sergi@datahop.network>
2022-01-22 16:23:16 +0100

    new sim script

--- service-discovery/run_exiting_topic_experiments.sh
+++ service-discovery/run_exiting_topic_experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SYBIL_SIZES='10'
-ATTACK_TOPICS='5'
+ATTACK_TOPICS='1 3 5'
 EVIL_PERCENTS='0.2'
 
 IN_CONFIG='config/existingTopicAttack.cfg'
@@ -15,7 +15,7 @@ function run_sim(){
     sed -i "s/^init.1uniqueNodeID.nodeIdSize .*$/init.1uniqueNodeID.nodeIdSize $SYBIL_SIZE/g" $OUT_CONFIG
     sed -i "s/^init.1uniqueNodeID.attackTopic .*$/init.1uniqueNodeID.attackTopic $TOPIC/g" $OUT_CONFIG
     sed -i "s/^init.1uniqueNodeID.percentEvil .*$/init.1uniqueNodeID.percentEvil $PERCENT_EVIL/g" $OUT_CONFIG
-    LOG_FILE="logs_attackTopic${TOPIC}_sybilSize${SYBIL_SIZE}_attackPercent${PERCENT_EVIL}_randomsearch"
+    LOG_FILE="logs_attackTopic${TOPIC}_sybilSize${SYBIL_SIZE}_attackPercent${PERCENT_EVIL}"
     echo log file $LOG_FILE
     sed -i "s/^control.3.rangeExperiment .*$/control.3.rangeExperiment $LOG_FILE/g" $OUT_CONFIG
     ./run.sh $OUT_CONFIG &> /dev/null
