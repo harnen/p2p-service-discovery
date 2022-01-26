b067ff80fb51fb2372d8c0b1c41baf78e789ef47
Sergi Rene <sergi@datahop.network>
2022-01-26 10:30:45 +0100

    config file update

--- service-discovery/run_exiting_topic_experiments.sh
+++ service-discovery/run_exiting_topic_experiments.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
 
-SYBIL_SIZES='10'
-ATTACK_TOPICS='1 3 5'
+ATTACK_TOPICS='3 1'
 EVIL_PERCENTS='0.2'
+UNIFORM=("nonUniform")
 
 IN_CONFIG='config/existingTopicAttack.cfg'
 
+
 function run_sim(){
-    OUT_CONFIG="config/attackspam_topic${TOPIC}_sybilsize${SYBIL_SIZE}_attackerPercent${PERCENT_EVIL}.cfg"
+    OUT_CONFIG="config/discv5_attackspam_topic${TOPIC}_${UNIFORM}_attackerPercent${PERCENT_EVIL}.cfg"
 	echo output config $OUT_CONFIG
 	cp $IN_CONFIG $OUT_CONFIG
     dos2unix $OUT_CONFIG
-    sed -i "s/^init.1uniqueNodeID.iPSize .*$/init.1uniqueNodeID.iPSize $SYBIL_SIZE/g" $OUT_CONFIG
-    sed -i "s/^init.1uniqueNodeID.nodeIdSize .*$/init.1uniqueNodeID.nodeIdSize $SYBIL_SIZE/g" $OUT_CONFIG
     sed -i "s/^init.1uniqueNodeID.attackTopic .*$/init.1uniqueNodeID.attackTopic $TOPIC/g" $OUT_CONFIG
     sed -i "s/^init.1uniqueNodeID.percentEvil .*$/init.1uniqueNodeID.percentEvil $PERCENT_EVIL/g" $OUT_CONFIG
-    LOG_FILE="logs_attackTopic${TOPIC}_sybilSize${SYBIL_SIZE}_attackPercent${PERCENT_EVIL}"
+    sed -i "s/^init.1uniqueNodeID.idDistribution .*$/init.1uniqueNodeID.idDistribution $UNI/g" $OUT_CONFIG
+    
+    LOG_FILE="logs_discv5_attackTopic${TOPIC}_${UNI}_attackPercent${PERCENT_EVIL}"
     echo log file $LOG_FILE
     sed -i "s/^control.3.rangeExperiment .*$/control.3.rangeExperiment $LOG_FILE/g" $OUT_CONFIG
     ./run.sh $OUT_CONFIG &> /dev/null
 }
 
-for SYBIL_SIZE in $SYBIL_SIZES
+
+for TOPIC in $ATTACK_TOPICS
 do
-	echo running size $SYBIL_SIZE
-    for TOPIC in $ATTACK_TOPICS
+    echo running attack topic $TOPIC
+    for PERCENT_EVIL in $EVIL_PERCENTS
     do
-	    echo running attack topic $TOPIC
-        for PERCENT_EVIL in $EVIL_PERCENTS
+        echo running percent evil $PERCENT_EVIL
+        for UNI in ${UNIFORM[@]};
         do
-	        echo running percent evil $PERCENT_EVIL
-            run_sim
+        	echo running uniform $UNI
+        	run_sim
         done
     done
 done
