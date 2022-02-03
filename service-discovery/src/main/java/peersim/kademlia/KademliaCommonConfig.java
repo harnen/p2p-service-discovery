308e7e40d1c8b52ddde9f1e4098db3ddcffe66f6
Sergi Rene <sergi@datahop.network>
2022-02-03 15:33:44 +0100

    dht stopping condition

--- service-discovery/src/main/java/peersim/kademlia/KademliaCommonConfig.java
+++ service-discovery/src/main/java/peersim/kademlia/KademliaCommonConfig.java
@@ -22,7 +22,7 @@ public class KademliaCommonConfig {
     public static int AD_LIFE_TIME = 250000; //life time of ads the topic table
     public static int MAX_REGISTRATION_RETRIES = 10; //life time of ads the topic table
     public static int ONE_UNIT_OF_TIME = 1; // smallest time value
-    public static int TOPIC_PEER_LIMIT = 16; //how many high quality nodes do we want to find for each topic; after this number we stop
+    public static int TOPIC_PEER_LIMIT = (int)KademliaCommonConfig.MAXCONNECTIONS/3; //how many high quality nodes do we want to find for each topic; after this number we stop
     public static int MAX_SEARCH_HOPS = 50; //how many high quality nodes do we want to find for each topic; after this number we stop
     public static int MAX_TICKETS = 100;
     public static int MAX_REG_BUCKETS = 0;
