91b4b33708c02e5b92fb9c9afc57632208382f94
oascigil <o.ascigil@ucl.ac.uk>
2022-03-21 16:30:53 +0000

    security plot generation

--- service-discovery/python/nsdi_plots.py
+++ service-discovery/python/nsdi_plots.py
@@ -13,6 +13,17 @@ import os
 
 csv.field_size_limit(sys.maxsize)
 
+# used for eclipsing results
+def getPercentEvilFromPath(path):
+    return path.split('percentEvil-')[1].split('_')[0].replace('/', '')
+def getSybilSizeFromPath(path):
+    return path.split('sybil_size-')[1].split('_')[0].replace('/', '')
+def getAttackTopicFromPath(path):
+    return path.split('attackTopic-')[1].split('_')[0].replace('/', '')
+def getProtocolFromPath(path):
+    return  path.split('/')[0]
+def getIdDistributionFromPath(path):
+    return path.split('id_distribution-')[1].split('_')[0].replace('/', '')
 
 def getProtocolFromPath(path):
     return  path.split('/')[0]
@@ -73,6 +84,57 @@ def createPerNodeStats(dir):
     dfs.to_csv('dfs.csv')
     return dfs
 
+# Used for eclipsing results
+def createPerLookupOperationStats(dir):
+    df_list = []
+    object_list = os.listdir(dir)
+    dirs = []
+    for obj in object_list:
+        if(os.path.isdir(obj)):
+            dirs.append(obj)
+    
+    for log_dir in dirs:
+        print("log_dir:", log_dir)
+        tmp = next(os.walk(log_dir))
+        print("tmp:", tmp)
+        sub_dirs = tmp[1]
+        for subdir in sub_dirs:
+            path = log_dir + '/' + subdir + '/'
+            path.replace('//','/')
+            print('Reading folder ', path)
+            try:
+                df = pd.read_csv(path + 'eclipse_counts.csv')
+                protocol = getProtocolFromPath(path)
+                sybilSize = getSybilSizeFromPath(path)
+                attackTopic = getAttackTopicFromPath(path)
+                distribution = getIdDistributionFromPath(path)
+                percentEvil = getPercentEvilFromPath(path)
+                print("From path:", path, "Extracted protocol:", protocol, "sybil size:", sybilSize, "attack topic:", attackTopic, "ID distribution: ",distribution )
+
+                percentDiscoveredField = 'PercentEvilDiscovered-t' + attackTopic
+                percentEclipsedField = 'PercentEclipsed-t' + attackTopic
+                df = df.loc[~(df[percentDiscoveredField] == 0)]
+                print('Series: ', df[percentDiscoveredField])
+                print('Here')
+                df['PercentEvilDiscovered'] = df[percentDiscoveredField]
+                df['PercentEclipsed'] = df[percentEclipsedField] 
+                df['percentEvil'] = percentEvil
+                
+                df['protocol'] = protocol
+                df['sybilSize'] = sybilSize
+                df['attackTopic'] = attackTopic
+                df['distribution'] = distribution
+                 
+                df_list.append(df)
+                df.to_csv(path + 'df.csv')
+            except FileNotFoundError:
+                print("Error: ", path, "eclipse_counts.csv not found")
+                quit()
+    #merge all the dfs
+    dfs = pd.concat(df_list, axis=0, ignore_index=True)
+    print(dfs)
+    dfs.to_csv('dfs.csv')
+    return dfs
 
 def plotPerNodeStats(bar=True):
     pd.set_option('display.max_rows', None)
@@ -124,6 +186,38 @@ def plotPerNodeStats(bar=True):
             else:
                 fig.savefig(OUTDIR + '/' + 'bar_'  + feature + "_" + graph)
 
+def plotPerLookupOperation():
+    pd.set_option('display.max_rows', None)
+    dfs = pd.read_csv('dfs.csv')
+    features = ['sybilSize', 'attackTopic', 'distribution', 'percentEvil']
+    #default values for all the features
+    defaults = {'sybilSize':'5', 'attackTopic':'1', 'distribution':'uniform', 'percentEvil':'0.2'}
+    for feature in features:
+        defaults[feature] = dfs[feature].value_counts().idxmax()
+
+    #x-axis
+    for feature in features:
+        #make sure we don't modify the initial df
+        df = dfs.copy(deep=True)
+        #filter the df so that we only have default values for non-primary features
+        for secondary_feature in features:
+            if(secondary_feature != feature):
+                df = df[df[secondary_feature] == defaults[secondary_feature]]
+        #y-axis
+        for graph in ['PercentEvilDiscovered', 'PercentEclipsed']:
+            fig, ax = plt.subplots()
+            for protocol, group in df.groupby('protocol'):
+                #NaN -> 0
+                group = group.fillna(0)
+                avg = group.groupby(feature)[graph].mean()
+                std = group.groupby(feature)[graph].std()
+                bx = avg.plot(x=feature, y=graph, yerr=std, ax=ax, legend=True, label=protocol)
+                bx.set_xlabel(feature)
+                bx.set_ylabel("Average " + graph)
+                bx.set_title(graph)
+                
+            fig.savefig(OUTDIR + '/' + feature + "_" + graph)
+
 OUTDIR = './'
 
 if len(sys.argv) < 2:
@@ -140,9 +234,8 @@ if not os.path.exists(OUTDIR):
 print('Will read logs from', LOGDIR)
 print('Plots will be saved in ', OUTDIR)
 
+createPerLookupOperationStats(LOGDIR)
+plotPerLookupOperation()
 
-
-createPerNodeStats(LOGDIR)
-plotPerNodeStats()
-
-
+#createPerNodeStats(LOGDIR)
+#plotPerNodeStats()
