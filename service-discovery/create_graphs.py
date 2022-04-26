dd96423aff7f8d08b9560e2c6513253f596ad902
Michał Król <mharnen@gmail.com>
2022-04-26 22:20:06 +0100

    refactor running/plotting scripts

---
+++ service-discovery/create_graphs.py
@@ -0,0 +1,35 @@
+import sys
+from python.nsdi_plots import *
+
+INDIR_BASE = './python_logs/'
+OUTDIR_BASE = './plots/'
+
+INDIR_BASE = os.path.abspath(INDIR_BASE)
+OUTDIR_BASE = os.path.abspath(OUTDIR_BASE)
+#if len(sys.argv) < 2:
+#    print('Usage: ', sys.argv[0], ' <Path_to_Log_files> <OPTIONAL: Path_to_output_dir>' )
+#    sys.exit(1) 
+
+#LOGDIR = sys.argv[1]
+#if len(sys.argv) > 2:
+#    OUTDIR = sys.argv[2]
+
+
+
+#createPerLookupOperationStats(LOGDIR)
+#plotPerLookupOperation()
+for simulation_type in ['benign', 'attack']:
+    INDIR = INDIR_BASE + "/" + simulation_type + "/"
+    OUTDIR = OUTDIR_BASE + "/" + simulation_type + "/"
+    
+    print("#######################" + simulation_type + "#######################")
+    print('Will read logs from', INDIR)
+    print('Plots will be saved in ', OUTDIR)
+
+    if not os.path.exists(OUTDIR):
+        os.makedirs(OUTDIR)
+
+    os.chdir(INDIR)
+    
+    createPerNodeStats(".")
+    plotPerNodeStats(OUTDIR, simulation_type)
